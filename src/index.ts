0da4184a1aa3f808b2136ea1280781496be7654d
Gwenaël Magnenat <g.magnenat@gmail.com>
2021-12-19 23:50:22 +0100

    Setup graphql and apollo server

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import { __prod__ } from './constants';
 import { Post } from './entitites/Post';
 import microConfig from './mikro-orm.config';
 import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
 
 const main = async () => {
   // connect to database
@@ -12,9 +15,16 @@ const main = async () => {
 
   const app = express();
 
-  app.get('/', (_, res) => {
-    res.send('Hello');
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
   });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app });
 
   app.listen(4000, () => {
     console.log('server started on localhost:4000');
