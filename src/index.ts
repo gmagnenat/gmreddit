2953f49a6fc79ead4edac6a7cc5e16491527359b
Gwenaël Magnenat <g.magnenat@gmail.com>
2021-12-19 23:20:01 +0100

    Init application, add typescript, configure mikro-orm

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+import { Post } from './entitites/Post';
+import microConfig from './mikro-orm.config';
+
+const main = async () => {
+  // connect to database
+  const orm = await MikroORM.init(microConfig);
+
+  // run the migration
+  await orm.getMigrator().up();
+
+  // const post = orm.em.create(Post, { title: 'my first post' });
+  // await orm.em.persistAndFlush(post);
+
+  // const posts = await orm.em.find(Post, {});
+  // console.log(posts);
+};
+
+main().catch((err) => {
+  console.error(err);
+});
