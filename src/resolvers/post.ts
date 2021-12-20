10fb50af04ae387cc5985d821164d7c2c7cc98eb
Gwenaël Magnenat <g.magnenat@gmail.com>
2021-12-20 08:19:44 +0100

    complete CRUD for post

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,19 +1,22 @@
-import { Resolver, Query, Ctx, Arg, Int, Mutation } from 'type-graphql';
+import { Resolver, Query, Ctx, Arg, Mutation } from 'type-graphql';
 import { MyContext } from 'src/types';
 import { Post } from '../entitites/Post';
 
 @Resolver()
 export class PostResolver {
+  // return all posts
   @Query(() => [Post])
   posts(@Ctx() { em }: MyContext): Promise<Post[]> {
     return em.find(Post, {});
   }
 
+  // return one post by its id
   @Query(() => Post, { nullable: true })
   post(@Arg('id') id: number, @Ctx() { em }: MyContext): Promise<Post | null> {
     return em.findOne(Post, { id });
   }
 
+  // create a new post
   @Mutation(() => Post)
   async createPost(
     @Arg('title') title: string,
@@ -24,12 +27,13 @@ export class PostResolver {
     return post;
   }
 
+  // update a post
   @Mutation(() => Post, { nullable: true })
   async updatePost(
     @Arg('id') id: number,
     @Arg('title', () => String, { nullable: true }) title: string,
     @Ctx() { em }: MyContext
-  ): Promise<Post> {
+  ): Promise<Post | null> {
     const post = await em.findOne(Post, { id });
     if (!post) {
       return null;
@@ -41,6 +45,7 @@ export class PostResolver {
     return post;
   }
 
+  // delete a post
   @Mutation(() => Boolean)
   async deletePost(
     @Arg('id') id: number,
