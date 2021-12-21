cef27b4c13c808419343dbd03de1b42cd8932367
Gwenaël Magnenat <g.magnenat@gmail.com>
2021-12-21 09:58:04 +0100

    Complete login migration and user resolver

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,13 +2,14 @@ import { Post } from './entitites/Post';
 import { __prod__ } from './constants';
 import { MikroORM } from '@mikro-orm/core';
 import path from 'path';
+import { User } from './entitites/User';
 
 export default {
   migrations: {
     path: path.join(__dirname, './migrations'),
     pattern: /^[\w-]+\d+\.[tj]s$/,
   },
-  entities: [Post],
+  entities: [Post, User],
   dbName: 'lireddit',
   password: 'postgres',
   type: 'postgresql',
